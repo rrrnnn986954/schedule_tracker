bdc02700fb57224f152e886fa84a088892f284ed
rrrnnn986954 <negima6954@gmail.com>
2025-11-03 15:01:25 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/new'
-  get 'categories/create'
-  get 'tracked_sessions/new'
-  get 'calendar/index'
-  get 'calendar/day'
   devise_for :users
   root "home#index"
+
+  # カレンダー（月→日）
+  get  "calendar",       to: "calendar#index", as: :calendar_index
+  get  "calendar/day",   to: "calendar#day",   as: :calendar_day  # ?date=YYYY-MM-DD
+
+  # タイムトラッカー（開始/終了/保存は今後実装）
+  resources :tracked_sessions, only: [:new, :create] do
+    collection do
+      post :start
+      post :stop
+    end
+  end
+
+  # カテゴリー
+  resources :categories, only: [:index, :new, :create]
 end
