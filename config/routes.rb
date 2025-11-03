7a0d6829a312d30bcf4e6424bcdd24c4e677d475
rrrnnn986954 <negima6954@gmail.com>
2025-11-03 15:47:40 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
   get "calendar",     to: "calendar#index", as: :calendar_index
   get "calendar/day", to: "calendar#day",   as: :calendar_day  # ?date=YYYY-MM-DD
 
+  resources :tracked_sessions, only: [:new, :create] do
+    collection do
+      post :start
+      post :stop
+    end
+  end
+
   # 計画（当日ページからPOST）
   resources :planned_events, only: [:create]
 
