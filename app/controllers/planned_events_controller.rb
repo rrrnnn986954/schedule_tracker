04acabe1a1965d39510b319bbfbb8008051c4d60
rrrnnn986954 <negima6954@gmail.com>
2025-11-03 17:08:30 +0900

    予定保存時、カテゴリー未選択なら「その他」を使う

--- app/controllers/planned_events_controller.rb
+++ app/controllers/planned_events_controller.rb
@@ -6,14 +6,20 @@ class PlannedEventsController < ApplicationController
     start_time = Time.zone.parse("#{date} #{params[:start_at]}")
     end_time   = Time.zone.parse("#{date} #{params[:end_at]}")
 
+    category = if params[:category_id].present?
+                 current_user.categories.find(params[:category_id])
+               else
+                 current_user.default_category
+               end
+
     planned = current_user.planned_events.new(
       start_at: start_time,
       end_at:   end_time,
-      category_id: params[:category_id]
+      category: category
     )
 
     if planned.save
-      redirect_to calendar_day_path(date: date), notice: "予定を追加しました"
+      redirect_to calendar_day_path(date: date), notice: "予定を追加しました（#{category.name}）"
     else
       redirect_to calendar_day_path(date: date), alert: planned.errors.full_messages.to_sentence
     end
