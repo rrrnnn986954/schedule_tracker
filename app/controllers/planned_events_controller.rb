f74f6a251ebe2dd17e70448f35230f61602cac33
rrrnnn986954 <negima6954@gmail.com>
2025-11-07 13:39:41 +0900

    コントローラー修正

--- app/controllers/planned_events_controller.rb
+++ app/controllers/planned_events_controller.rb
@@ -37,4 +37,52 @@ class PlannedEventsController < ApplicationController
       redirect_to calendar_day_path(date: date), alert: planned.errors.full_messages.to_sentence
     end
   end
+
+  # 追加: 編集フォーム
+  def edit
+    @date       = @planned_event.start_at.to_date
+    @categories = current_user.categories.order(:name)
+  end
+
+  # 追加: 更新
+  def update
+    date = @planned_event.start_at.to_date
+    start_at = parse_time_on(date, params[:start_at])
+    end_at   = parse_time_on(date, params[:end_at], allow_24: true)
+
+    if @planned_event.update(start_at: start_at, end_at: end_at, category_id: params[:category_id])
+      redirect_to day_calendar_index_path(date: date), notice: "予定を更新しました"
+    else
+      flash.now[:alert] = @planned_event.errors.full_messages.join(" / ")
+      @date       = date
+      @categories = current_user.categories.order(:name)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+  # 追加: 削除（任意）
+  def destroy
+    date = @planned_event.start_at.to_date
+    @planned_event.destroy
+    redirect_to day_calendar_index_path(date: date), notice: "予定を削除しました"
+  end
+
+  private
+
+  def set_planned_event
+    @planned_event = current_user.planned_events.find(params[:id])
+  end
+
+  # "HH:MM" を当日Dateに乗せて Time を返す
+  # allow_24: true かつ "24:00" のときは翌日00:00にする
+  def parse_time_on(date, hm, allow_24: false)
+    return nil if hm.blank?
+    h, m = hm.split(":").map!(&:to_i)
+    if allow_24 && h == 24 && m == 0
+      (date + 1).to_time.change(hour: 0, min: 0, sec: 0)
+    else
+      date.to_time.change(hour: h, min: m, sec: 0)
+    end
+  end
 end
